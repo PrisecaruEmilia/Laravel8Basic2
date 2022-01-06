8cf8ac5b5d0fc31b8e99c8bd6c1e0253a8577ae9
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-01-06 18:37:14 +0200

    Routing to a Single Controller Method

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,4 @@ Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/contact', [ContactController::class, 'index']);
