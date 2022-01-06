7b96b2328e5163f83408d1baa844f132eca4819c
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-01-06 20:17:04 +0200

    read data from DB with query builder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
@@ -31,6 +32,7 @@ Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
-    $users = User::all();
+    // $users = User::all();
+    $users = DB::table('users')->get();
     return view('dashboard', compact('users'));
 })->name('dashboard');
