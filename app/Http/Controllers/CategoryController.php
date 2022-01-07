341dec18fa23eae992866650b9de3dfbedaf5816
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-01-07 18:34:09 +0200

    insert data eloquent ORM -- category --

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class CategoryController extends Controller
 {
@@ -22,5 +26,24 @@ class CategoryController extends Controller
                 'category_name.min' => "Category name must be between 5 and 255",
             ]
         );
+
+        // insert data eloquent ORM
+        // 1 approach
+
+        Category::insert([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id,
+            'created_at' => Carbon::now(),
+        ]);
+
+        // 2 approach
+
+        // $category = new Category;
+        // $category->category_name = $request->category_name;
+        // $category->user_id = Auth::user()->id;
+        // $category->created_at = Carbon::now();
+        // $category->save();
+
+        return Redirect()->back()->with('success', 'Category created successfully.');
     }
 }
